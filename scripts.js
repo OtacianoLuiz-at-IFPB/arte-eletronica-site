b5dfd6cc519022bf9b97560545c52807b7a2d456
Otaciano Luiz <otaciano.luiz@academico.ifpb.edu.br>
2022-10-13 01:31:35 -0300

    changes were made

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 document.getElementById('lightButton').addEventListener("click", changeLightmode);
+document.getElementById('showNav').addEventListener("click", changeNav);
+
 let light = "night";
 let a = getComputedStyle(html).getPropertyValue("--default-color");
 let b = getComputedStyle(html).getPropertyValue("--default-background-color");
@@ -10,5 +12,15 @@ function changeLightmode(){
     document.documentElement.style.setProperty("--default-background-color", b);
     [lightmode,notlightmode] = [notlightmode,lightmode];
     document.getElementById("lightButton").innerHTML = lightmode;
-    
+}
+
+function changeNav(){
+    var lateralbar = document.getElementsByTagName("aside")[0];
+    if (lateralbar.style.display === "none") {
+        lateralbar.style.display = "block";
+        document.getElementById("showNav").innerHTML = "<";
+    } else {
+        lateralbar.style.display = "none"
+        document.getElementById("showNav").innerHTML = ">"
+    }
 }
